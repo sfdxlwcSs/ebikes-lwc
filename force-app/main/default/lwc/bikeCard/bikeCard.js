b4cc2d89fbbd94e512a9a36dcc435d05cf999a1f
Somnath Sharma <52417989+sfdxlwcSs@users.noreply.github.com>
2023-03-02 12:25:17 +0530

    sync

---
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -0,0 +1,16 @@
+/**
+ * @description       : 
+ * @author            : Somnath Sharma
+ * @group             : 
+ * @last modified on  : 02-03-2023
+ * @last modified by  : Somnath Sharma
+**/
+import { LightningElement } from 'lwc';
+export default class BikeCard extends LightningElement {
+    name = 'Electra X4';
+    description = 'A sweet bike built for comfort.';
+    category = 'Mountain';
+    material = 'Steel';
+    price = '$2,700';
+    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+}
