66aee54013b0b75d3b38c3a2f06c489124c51ae4
Somnath Sharma <52417989+sfdxlwcSs@users.noreply.github.com>
2023-04-26 20:08:20 +0530

    badges

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,30 @@
+/**
+ * @description       : 
+ * @author            : Somnath Sharma
+ * @group             : 
+ * @last modified on  : 26-04-2023
+ * @last modified by  : Somnath Sharma
+**/
+import { LightningElement, wire } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
+const COLUMNS = [
+    { label: 'First Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Last Name', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+];
+
+export default class ContactList extends LightningElement {
+
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
+}
